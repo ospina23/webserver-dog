f1655dbfd75212ad6d4ce3eb5ba7b2edd8a95a8f
ospina23 <ospina.23@hotmail.com>
2019-08-08 17:55:14 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'))
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Jose L Ospina'
+    })
+});
+
+app.get('/nosotros', (req, res) => {
+    res.render('nosotros')
+});
+
+app.get('/data', (req, res) => {
+    res.send('Hola data')
+
+})
+
+app.listen(port, () => { console.log(`Escuchando puerto ${port}`) })
